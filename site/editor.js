ec4cff5c0adef075f1c82326918754702b21f707
Michał Swoboda <michal.swoboda@interia.pl>
2023-11-23 17:14:12 +0100

    Editor changes FUKING COLORING CAN'T WORK

--- site/editor.js
+++ site/editor.js
@@ -1,12 +1,34 @@
+/*function getCursorPosition(contentEditableElement) {
+    let range = window.getSelection().getRangeAt(0);
+    let preCaretRange = range.cloneRange();
+    preCaretRange.selectNodeContents(contentEditableElement);
+    preCaretRange.setEnd(range.endContainer, range.endOffset);
+    return preCaretRange.toString().length;
+}
+function setCursorPosition(contentEditableElement, position) {
+    if (contentEditableElement.childNodes.length > 0) {
+        let range = document.createRange();
+        let selection = window.getSelection();
+        position = Math.min(position, contentEditableElement.childNodes[0].length);
+        range.setStart(contentEditableElement.childNodes[0], position);
+        range.collapse(true);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        contentEditableElement.focus();
+    }
+}
 function codeEditorColors(x){
     let keywords1 = ["clear","clone","deop","difficulty","effect","enchant","execute","fill","function","gamemode","gamerule","give","help","kick","kill","list","locate","me","msg","op","particle","playsound","reload","say","scoreboard","setblock","setworldspawn","spawnpoint","spreadplayers","stopsound","summon","tag","teleport","tell","tellraw","time","title","tp","w","weather","xp","return","advancement","attribute","ban","ban-ip","banlist","bossbar","damage","data","datapack","debug","defaultgamemode","experience","fillbiome","forceload","item","jfr","loot","pardon","pardon-ip","perf","place","publish","random","recipe","ride","save-all","save-off","save-on","schedule","seed","setidletimeout","spectate","stop","team","teammsg","tm","trigger","whitelist","worldborder"]
-    let content = x.innerHTML//.replaceAll("<span>", "").replaceAll("</span>", "").replaceAll("<div>", "").replaceAll("<br>", "").replaceAll("</div>", "\n")
-    keywords1.forEach(element => {
-        content = content.replaceAll(element, '<span class="kw1">'+element+'</span>')
+    let pos = getCursorPosition(x)
+    let content = x.innerText;
+    keywords1.forEach(function(keyword){
+        let regex = new RegExp("\\b" + keyword + "\\b", "g");
+        content = content.replace(regex, '<span class="kw1">'+keyword+'</span>');
     });
-    console.log(content)
-    x.innerHTML = "<div>"+content.replaceAll("\n", "</div><div>")+"</div>"
-}
+    x.innerHTML = content;
+    setCursorPosition(x, pos);
+}*/
+function codeEditorColors(x){}
 function switchOpen(x){
     let parentElement = x.parentElement
     let nextSibling = parentElement.nextElementSibling
